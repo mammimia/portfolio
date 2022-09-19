6a08e9aa99fb0783a71fc23b10d91dcbde5c2771
Muhammed Ali <muhammedali@d14.ai>
2022-09-19 19:30:18 +0300

    Header and Hero implemented

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+type Props = {};
+
+function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hey, I'm Muhammed Ali AYDIN",
+      "<OneOfTheMostAmbitiousMenYouCanKnow />",
+      "Guys-who-is-obsessed-with-Coffee.tsx",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://pps.whatsapp.net/v/t61.24694-24/158079383_268114754972725_8449800354676983179_n.jpg?ccb=11-4&oh=01_AVx4IhqCrpdExqQSDc4xnMmEALzMPva93aOF3zpoDMYXaA&oe=633973BA"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Full Stack Developer</h2>
+        <h1 className="text-5xl lg:6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="$F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="hero-button">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="hero-button">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="hero-button">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="hero-button">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
