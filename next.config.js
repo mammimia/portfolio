9478bba1be0002ace28627c60b61a6aa51d60fc7
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-01 13:36:21 +0300

    Config file changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["picsum.photos"],
+  },
+  assetPrefix: isProd ? "https://mammimia.github.io/portfolio/" : "",
+  images: {
+    unoptimized: true,
   },
 };
 
